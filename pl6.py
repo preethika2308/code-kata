6b4c591f33eb0d0cbf89e0cb970c8160dbd36ead
preethika2308 <51670057+preethika2308@users.noreply.github.com>
2019-07-01 10:54:27 -0700

    Create pl6.py

---
+++ pl6.py
@@ -0,0 +1,11 @@
+lh=[str(i) for i in input().split()]
+s1=lh[0]
+s2=lh[1]
+if(len(s1)!=len(s2)):
+    print("no")
+a1=[s1.count(ch) for ch in s1]
+b1=[s2.count(ch) for ch in s2]
+if(a1==b1):
+    print("yes")
+else:
+    print("no")
